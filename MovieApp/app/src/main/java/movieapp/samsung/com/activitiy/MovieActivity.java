2d1030523e42d7370a518f427a813acac3153a56
vikram-tandon-4 <vikramtandon4192@gmail.com>
2018-03-14 11:55:21 -0500

    First Commit
    1.Added toolbar
    2. Added Movie Activity

---
+++ MovieApp/app/src/main/java/movieapp/samsung/com/activitiy/MovieActivity.java
@@ -0,0 +1,15 @@
+package movieapp.samsung.com.activitiy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import movieapp.samsung.com.R;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+    }
+}
